652a488a2c76f686ddd020029cba4e422d5b2acb
Ross <Theadora.Ross@tyson.com>
2019-06-21 10:54:44 -0500

    debug

--- get_data.py
+++ get_data.py
@@ -4,9 +4,13 @@ from git import Repo
 
 # Load environment vars
 load_dotenv(find_dotenv())
+print("Loading environment variables")
 REPO_DIR = os.getenv("REPO_DIR")
+print("Directory for found repositories: %s" % REPO_DIR)
 LABELED_DATA_DIR = os.getenv("LABELED_DATA_DIR")
+print("Directory for labeled manifest and abap files: %s" % LABELED_DATA_DIR)
 MANIFEST_FILE = os.getenv("MANIFEST_FILE")
+print("File path for manifest: %s" % MANIFEST_FILE)
 
 # Check Github for public repositories of language ABAP that is at least 100 KB in size
 REPO_SIZE_GT = 100 # Size in kilobytes of repository
@@ -21,6 +25,7 @@ def clone_repos() -> list:
     repo_list = []
 
     r = requests.get(URL, headers=HEADERS, params=PARAMS)
+    print("Running query: {}".format(r.url))
     data = r.json()
     print("Total number of repositories: {}".format(data['total_count']))
 
@@ -30,6 +35,7 @@ def clone_repos() -> list:
         repo_dir = "{}/{}_{}".format(REPO_DIR, repo['owner']['login'], repo['name'])
 
         # append to list
+        print("Adding repository to list %s" % repo_dir)
         repo_list.append(repo_dir)
 
         # clone
@@ -72,9 +78,14 @@ def extract_files_from_repos(repo_dirs) -> dict:
             # Find .abap files in repo
             for fname in fnames:
                 if fname.split(".")[-1] == "abap":
+                    print("Found abap source code: %s" % fname)
                     # cp file to raw data directory
                     orig_fname = os.path.join(os.path.abspath(root), fname)
+                    print("Original File Path %s" % orig_fname)
                     new_fname = os.path.join(os.path.abspath(os.getenv(LABELED_DATA_DIR)), fname)
+                    print("Target file path %s" % new_fname)
+
+                    print("Copying file {} to data directory {}".format(fname, LABELED_DATA_DIR))
                     shutil.copy(orig_fname, new_fname)
 
                     # Write line for datapoint 'author,project,file_path,\n'
@@ -84,7 +95,8 @@ def extract_files_from_repos(repo_dirs) -> dict:
 
 def main():
     repo_list = clone_repos()
-    manifest = extract_files_from_repos(repo_list)
+    print("Got repo list, cloning and extracting")
+    manifest = extract_files_from_repos(repo_list[0:5])
 
     with open(os.path.abspath(MANIFEST_FILE), "w", encoding="utf-8") as f:
         json.dump(manifest, f, ensure_ascii=False, indent=2)
