77f6d7057545bafd982c7fb1629e73abaea2c61a
Thea Ross <taross1993@gmail.com>
2019-06-23 11:53:29 -0500

    Fixed github search query

---
+++ GetRawData/get_data.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+
+	rq "github.com/txross1993/abap-authorship-classifier/GetRawData/repo_requests"
+
+	dotenv "github.com/joho/godotenv"
+	git "gopkg.in/src-d/go-git.v4"
+)
+
+var MANIFEST_FILE = ""
+var WRITES_CHAN = make(chan []byte, 255)
+var WAIT_GROUP sync.WaitGroup
+
+type ManifestLabel struct {
+	author  string `json:"author"`
+	project string `json:"project"`
+	fileRef string `json:"file_ref"`
+}
+
+func copyFile(sourceFile string, destinationFile string) {
+	input, err := ioutil.ReadFile(sourceFile)
+	if err != nil {
+		fmt.Printf("\t\tError reading source abap file: %v\n", err)
+		return
+	}
+
+	err = ioutil.WriteFile(destinationFile, input, 0755)
+	if err != nil {
+		fmt.Printf("\t\tError creating abap file in destination dir %v\n", destinationFile)
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("\tCopying abap file to data directory successful! %v", destinationFile)
+}
+
+func getRawData(repoDir string, label *ManifestLabel) error {
+	// Copy any abap files to the raw data directory
+	err := filepath.Walk(repoDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		if strings.HasSuffix(path, ".abap") {
+			sourceFile, _ := filepath.Abs(path)
+			fmt.Printf("\tFound abap data! %v\n", sourceFile)
+			destinationFile, _ := filepath.Abs(fmt.Sprintf("%v/%v", os.Getenv("LABELED_DATA_DIR"), filepath.Base(path)))
+			copy_file(sourceFile, destinationFile)
+
+			// Annotate manifest
+			label.file_ref = destinationFile
+			data, _ := json.Marshal(label)
+			fmt.Printf("\t\tWriting to manifest channel: %v\n", label)
+			write_to_manifest_chan(data)
+
+		}
+		return nil
+	})
+
+	return err
+}
+
+func writeToManifestChan(data []byte) {
+	// Write label to manifest file
+	var b bytes.Buffer
+	b.Write([]byte(data))
+	b.Write([]byte("\n"))
+	WRITES_CHAN <- b.Bytes()
+}
+
+func cloneRepo(url string, destination string) error {
+	_, err := git.PlainClone(destination, false, &git.CloneOptions{
+		URL: url,
+	})
+
+	fmt.Printf("Cloned repository %v\n", url)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func cloneAndCopy(repo_url string, dest string, label *ManifestLabel) error {
+	defer WAIT_GROUP.Done()
+	if _, err := os.Stat(dest); os.IsNotExist(err) {
+		// If the repo directory does not exist, clone it
+		err := cloneRepo(repo_url, dest)
+		if err != nil {
+			return err
+		}
+	}
+	err := getRawData(dest, label)
+	return err
+}
+
+func CloneAllAndCopy(in []Repo, destDir string) {
+	for _, repo := range in {
+		label := &ManifestLabel{
+			author:  repo.Owner.User,
+			project: repo.ProjectName,
+			fileRef: "",
+		}
+		dest := destDir + "/" + repo.ProjectName
+		fmt.Printf("Initiating goroutine for repo %v", repo.ProjectName)
+		go cloneAndCopy(repo.CloneUrl, dest, label)
+	}
+	return
+}
+
+func main() {
+	// Load env vars
+	dotenv.Load()
+	REPO_DIR := os.Getenv("REPO_DIR")
+	MANIFEST_FILE, _ := filepath.Abs(os.Getenv("MANIFEST_FILE"))
+	REPO_SIZE_KB, _ := strconv.Atoi(os.Getenv("REPO_SIZE_KB"))
+
+	// Search for repositories
+	keyword_search := []string{"*", "abap", "sap", "program"}
+	allRepos := rq.GetAbapRepos(keyword_search, REPO_SIZE_KB)
+
+	WAIT_GROUP.Add(len(allRepos))
+	fmt.Println("Got all repositories . . . Beginning cloning and labeling")
+
+	/*
+		Clone all repositories into data dir
+		Traverse repositories, find .abap files, and move them to LABELED_DATA_DIR
+		Annotate manifest with file name, github author, and project
+	*/
+
+	// Clone
+
+	CloneAllAndCopy(allRepos, REPO_DIR)
+
+	// Write channel to manifest file
+	f, _ := os.Create(MANIFEST_FILE)
+	bw := bufio.NewWriter(f)
+
+	defer f.Close()
+	defer close(WRITES_CHAN)
+
+	go func() {
+		for {
+			data := <-WRITES_CHAN
+			fmt.Printf("Retrieved data from channel %v\n", data)
+			_, err := bw.Write(data)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+			if bw.Available() < 64 {
+				bw.Flush()
+			}
+		}
+
+	}()
+
+	WAIT_GROUP.Wait()
+
+}
---
+++ GetRawData/repo_requests/query.go
@@ -0,0 +1,142 @@
+package repo_requests
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sort"
+	"strings"
+)
+
+type Repo struct {
+	Id       int     `json:"id"`
+	Name     string  `json:"name"`
+	FullName string  `json:"full_name"`
+	CloneUrl string  `json:"clone_url"`
+	Score    float64 `json:"score"`
+	Language string  `json:"language"`
+}
+
+func (r *Repo) String() string {
+	return fmt.Sprintf("Id: %v\n\tName:%v\n\tScore:%v\n\tLanguage:%v\n\n", r.Id, r.FullName, r.Score, r.Language)
+}
+
+type ByRepoId []Repo
+
+func (brip ByRepoId) Len() int           { return len(brip) }
+func (brip ByRepoId) Swap(i, j int)      { brip[i], brip[j] = brip[j], brip[i] }
+func (brip ByRepoId) Less(i, j int) bool { return brip[i].Id < brip[j].Id }
+
+type GitHubRepoResponse struct {
+	Total int    `json:"total_count"`
+	Items []Repo `json:"items"`
+}
+
+func (g *GitHubRepoResponse) FilterOnlyAbapRepos() {
+	filter := make(map[int]struct{}, len(g.Items))
+
+	sort.Sort(ByRepoId(g.Items))
+
+	var repos []Repo
+	tmp := repos[:0]
+
+	// First, filter for abap repos
+	for _, repo := range g.Items {
+		if strings.ToLower(repo.Language) == "abap" {
+			filter[repo.Id] = struct{}{}
+		}
+
+		//If the repo Id exists in the filter, add it because it's abap
+		if _, ok := filter[repo.Id]; ok {
+			tmp = append(tmp, repo)
+		}
+	}
+
+	g.Items = tmp
+
+}
+
+func getRepoUrls(keyword string, repoSizeKb int, target *GitHubRepoResponse) *GitHubRepoResponse {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://api.github.com/search/repositories", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Accept", "application/vnd.github.mercy-preview+json")
+
+	q := req.URL.Query()
+
+	params := fmt.Sprintf("%v+language:ABAP+size:>=%d+is:public", keyword, repoSizeKb)
+
+	q.Add("q", params)
+
+	req.URL.RawQuery = q.Encode()
+
+	fmt.Printf("Querying for repos: %v\n", req.URL)
+	resp, respErr := client.Do(req)
+
+	if respErr != nil {
+		log.Fatal(respErr)
+	}
+
+	defer resp.Body.Close()
+
+	json.NewDecoder(resp.Body).Decode(&target)
+
+	return target
+
+}
+
+func uniqueAbapRepos(r []Repo) []Repo {
+	encountered := map[int]bool{}
+	var uniqueRepos []Repo
+
+	for _, repo := range r {
+		if encountered[repo.Id] == true {
+			// Don't add if already found
+		} else {
+			uniqueRepos = append(uniqueRepos, repo)
+			encountered[repo.Id] = true
+		}
+	}
+
+	return uniqueRepos
+}
+
+func GetAbapRepos(kws []string, gtkb int) []Repo {
+	/*
+		Args:
+			kws		[]string		Provide a slice of keywords to search by for repositories
+			gtkb	int				Provide the minimum size of repositories to search for in kb
+	*/
+
+	//Example keywords to search by
+	//kws := []string{"*", "abap", "sap", "transaction"}
+
+	var totalAbapRepos []Repo
+
+	for _, keyword := range kws {
+		target := new(GitHubRepoResponse)
+
+		getRepoUrls(keyword, gtkb, target)
+		target.FilterOnlyAbapRepos()
+
+		totalAbapRepos = append(totalAbapRepos, target.Items...)
+
+	}
+
+	totalAbapRepos = uniqueAbapRepos(totalAbapRepos)
+
+	lastIndex := len(totalAbapRepos) - 1
+
+	fmt.Printf("Found %v repos\n", len(totalAbapRepos))
+	for _, idx := range []int{0, lastIndex} {
+		fmt.Printf("Top Repo: \n%v", totalAbapRepos[idx].String())
+	}
+
+	return totalAbapRepos
+}
